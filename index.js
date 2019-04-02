1fd63d3bcf563288fe507f97a4c88012fa604a5f
LondonBishop <londonbishop@codecutters.co.uk>
2019-04-02 10:54:02 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+document.addEventListener("keydown", function(e) {
+    
+    switch (e.key) {
+        case "ArrowLeft": moveDodgerLeft();
+            break;
+        case "ArrowRight": moveDodgerRight();
+            break;
+    }
+    
+    console.log(e.key);
+});
+
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    var rNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(rNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+  }
